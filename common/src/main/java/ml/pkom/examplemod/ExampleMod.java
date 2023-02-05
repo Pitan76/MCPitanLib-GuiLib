171b18b2f86e2195bc99c83df3528dddb4ec4dd2
PTOM76 <ptms76@outlook.jp>
2023-02-05 18:27:17 +0900

    template

---
+++ common/src/main/java/ml/pkom/examplemod/ExampleMod.java
@@ -0,0 +1,8 @@
+package ml.pkom.examplemod;
+
+public class ExampleMod {
+    public static final String MOD_ID = "examplemod";
+
+    public static void init() {
+    }
+}
---
+++ fabric/src/main/java/ml/pkom/examplemod/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package ml.pkom.examplemod.fabric;
+
+import ml.pkom.examplemod.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
